ad64fba3ef0309326fe9a05e80b27e356979a7f1
chenhualong <chenhualong@sincere.com.cn>
2017-07-28 10:56:05 +0800

    提交Hystrix的监控系统用法

--- Study2-1-6/cloud-user-server/src/main/java/com/lemon/chen/CloudUserServerApplication.java
+++ Study2-1-6/cloud-user-server/src/main/java/com/lemon/chen/CloudUserServerApplication.java
@@ -2,11 +2,13 @@ package com.lemon.chen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //该注解能激活Eureka中的DiscoveryClient实现，这样才能实现Controller中对服务信息的输出
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class CloudUserServerApplication {
 
 	public static void main(String[] args) {
