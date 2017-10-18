725b0969b508d6db48a9a29a338703b0e095dc44
chenhualong <chenhualong@sincere.com.cn>
2017-10-18 10:21:00 +0800

    增加activity和笔记总纲

---
+++ Study-activity/src/main/java/com/lemon/activity/studyactivity/StudyactivityApplication.java
@@ -0,0 +1,12 @@
+package com.lemon.activity.studyactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyactivityApplication.class, args);
+	}
+}
