278a298bc99ed18553fb576e22423b751ad2b381
chenhualong <chenhualong@sincere.com.cn>
2017-11-14 17:48:39 +0800

    logback例子和用法

---
+++ Study-lemon-logback/src/main/java/com/lemon/logback/StudyLogback.java
@@ -0,0 +1,27 @@
+package com.lemon.logback;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author :chenhualong
+ * @Date : created in 17:44 2017/11/14
+ * @Description :
+ */
+
+@Slf4j
+public class StudyLogback {
+
+
+    public static void main(String[] args) {
+
+
+        log.info("log  info ");
+
+        log.warn("log  warn");
+
+        log.error("log error");
+
+
+    }
+
+}
