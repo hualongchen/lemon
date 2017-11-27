e7ee7d7ee109045d3e5f03c28e0f1322e453a9c1
chenhualong <chenhualong@sincere.com.cn>
2017-11-27 17:13:53 +0800

    v1

--- Study-springboot-all/src/main/java/com/lemon/springboot/study125/dao/mapper/user/SpringBootUserPOMapper.java
+++ Study-springboot-all/src/main/java/com/lemon/springboot/study125/dao/mapper/user/SpringBootUserPOMapper.java
@@ -13,6 +13,10 @@ public interface SpringBootUserPOMapper extends Mapper<SpringBootUserPO> {
      List<SpringBootUserPO>  findbyUserName(SpringBootUserPO  springBootUserPO);
 
 
+    @Select("select id,username,create_time,update_time from study_springboot_user where id=#{id}")
+    SpringBootUserPO  findbyUserName4(int id );
+
+
     @Select("select id,username,create_time,update_time from study_springboot_user where username=#{username} order by create_time desc")
      List<SpringBootUserPO>  findbyUserName2(SpringBootUserPO  springBootUserPO);
 
--- Study-springboot-all/src/main/java/com/lemon/springboot/study125/web/MybatisController.java
+++ Study-springboot-all/src/main/java/com/lemon/springboot/study125/web/MybatisController.java
@@ -6,6 +6,8 @@ import com.lemon.springboot.study125.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.entity.Example.Criteria;
 
 /**
  * @author chenhualong
@@ -41,14 +43,50 @@ public class MybatisController {
     }
 
 
+    @GetMapping("/mybatis6")
+    public Object test22(){
+
+
+        SpringBootUserPO  springBootUserPO = new SpringBootUserPO();
+
+        springBootUserPO.setUsername("通融");
+
+        Example  example = new Example(springBootUserPO.getClass());
+
+
+       example.setOrderByClause("create_time DESC");  //设置排序
+
+
+        Criteria criteria = example.createCriteria();
+
+        if(null != springBootUserPO.getUsername()){
+
+            criteria.andEqualTo("username",springBootUserPO.getUsername());
+        }
+
+       // criteria.andBetween("id",1,6);
+
+       // criteria.andGreaterThan("id",5);
+
+       // criteria.andLessThan("id",2);
+
+       // criteria.andIsNotNull("username");
+
+        //criteria.andCondition("group by id");
+
+        return springBootUserPOMapper.selectByExample(example);
+
+    }
+
+
     @GetMapping("/mybatis1")
     public Object  test1(){
 
         SpringBootUserPO  springBootUserPO = new SpringBootUserPO();
 
-        springBootUserPO.setUsername("通融3");
+        springBootUserPO.setUsername("通融");
 
-        springBootUserPO.setId(1);
+        springBootUserPO.setId(3);
 
         return  springBootUserPOMapper.findbyUserName(springBootUserPO);
     }
@@ -59,7 +97,7 @@ public class MybatisController {
 
         SpringBootUserPO  springBootUserPO = new SpringBootUserPO();
 
-        springBootUserPO.setUsername("通融3");
+        springBootUserPO.setUsername("通融");
         return  springBootUserPOMapper.findbyUserName2(springBootUserPO);
     }
 
@@ -70,8 +108,15 @@ public class MybatisController {
 
         SpringBootUserPO  springBootUserPO = new SpringBootUserPO();
 
-        springBootUserPO.setUsername("通融3");
+        springBootUserPO.setUsername("通融");
         return  springBootUserPOMapper.findbyUserName3(springBootUserPO);
+    }
+
+
+    @GetMapping("/mybatis4")
+    public Object  test1111(){
+
+        return  springBootUserPOMapper.findbyUserName4(1);
     }
 
 
